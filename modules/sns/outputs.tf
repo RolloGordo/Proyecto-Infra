40f97d7b7f12b5e6ea7717785a89f0e4db63b671
Fabrizzio Silva <hfsv123456@gmail.com>
2025-10-04 06:47:31 -0500

    fix: se hicieron pequeños cambios de variables, se hizo un terraform apply pero falló, después se aplicó un destroy.

--- modules/sns/outputs.tf
+++ modules/sns/outputs.tf
@@ -1,9 +1,9 @@
-output "sns_topic_arn" {
+output "topic_arn" {
   description = "ARN del SNS Topic creado"
   value       = aws_sns_topic.this.arn
 }
 
-output "sns_topic_name" {
+output "topic_name" {
   description = "Nombre del SNS Topic creado"
   value       = aws_sns_topic.this.name
 }
